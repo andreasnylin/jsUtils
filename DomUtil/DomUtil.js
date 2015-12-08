286ff8d3a08a0cd114f26795f5cf4928db6b0cf4
Andreas Nylin <andreas.nylin@gmail.com>
2015-12-08 15:54:31 +0100

    Added off-method to DomUtil

--- DomUtil/DomUtil.js
+++ DomUtil/DomUtil.js
@@ -1,4 +1,17 @@
 var DomUtil = {
+  addClass: function(element, className) {
+
+    if(element.classList) {
+      element.classList.add(className);
+    }
+    else {
+      var classes = element.className.split(' ');
+      if(classes.indexOf(className) === -1) {
+          classes.push(className);
+          element.className = classes.join(' ');
+      }
+    }
+  },
   get: function(sel) {
     return document.querySelector(sel);
   },
@@ -13,16 +26,39 @@ var DomUtil = {
 
     return el.children;
   },
+  off: function(element, eventName, handler) {
+    if (element.length) {
+      for (var i = 0, l = element.length; i < l; i++) {
+        element[i].removeEventListener(eventName, handler, false);
+      }
+    } else {
+      element.removeEventListener(eventName, handler, false);
+    }
+  },
   on: function(element, eventName, handler) {
     if (element.length) {
       for (var i = 0, l = element.length; i < l; i++) {
         element[i].addEventListener(eventName, handler, false);
       }
     } else {
-    element.addEventListener(eventName, handler, false);
-  }
+      element.addEventListener(eventName, handler, false);
+    }
   },
-  setStyle: function(style, element) {
+  removeClass: function(element, className) {
+    if(element.classList) {
+      element.classList.remove(className);
+    }
+    else {
+      var classes = element.className.split(' '),
+          classNameIndex = classes.indexOf(className);
+
+      if(classNameIndex != -1) {
+          classes.splice(classNameIndex, 1);
+          element.className = classes.join(' ');
+      }
+    }
+  },
+  setStyle: function(element, style) {
     for (var s in style) {
       element.style[s] = style[s];
     }
