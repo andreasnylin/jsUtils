6f769e19377a0d19e12e46a91d1829d99e0cba76
Andreas Nylin <andreas.nylin@gmail.com>
2016-02-03 08:31:21 +0100

    Updated math util

--- MathUtil/MathUtil.js
+++ MathUtil/MathUtil.js
@@ -1,5 +1,50 @@
 var MathUtil = {
 
+	norm: function(value, min, max) {
+		return (value - min) / (max - min);
+	},
+
+	lerp: function(norm, min, max) {
+		return (max - min) * norm + min;
+	},
+
+	map: function(value, sourceMin, sourceMax, destMin, destMax) {
+		return MathUtil.lerp(MathUtil.norm(value, sourceMin, sourceMax), destMin, destMax);
+	},
+
+	clamp: function(value, min, max) {
+		return Math.min(Math.max(value, Math.min(min, max)), Math.max(min, max));
+	},
+
+	distance: function(p0, p1) {
+		var dx = p1.x - p0.x,
+			dy = p1.y - p0.y;
+		return Math.sqrt(dx * dx + dy * dy);
+	},
+
+	distanceXY: function(x0, y0, x1, y1) {
+		var dx = x1 - x0,
+			dy = y1 - y0;
+		return Math.sqrt(dx * dx + dy * dy);
+	},
+
+	circleCollision: function(c0, c1) {
+		return utils.distance(c0, c1) <= c0.radius + c1.radius;
+	},
+
+	circlePointCollision: function(x, y, circle) {
+		return utils.distanceXY(x, y, circle.x, circle.y) < circle.radius;
+	},
+
+	pointInRect: function(x, y, rect) {
+		return utils.inRange(x, rect.x, rect.x + rect.width) &&
+		       utils.inRange(y, rect.y, rect.y + rect.height);
+	},
+
+	inRange: function(value, min, max) {
+		return value >= Math.min(min, max) && value <= Math.max(min, max);
+	},
+
 	degreesToRadians: function(degrees) {
     	return (degrees * Math.PI) / 180;
 	},
